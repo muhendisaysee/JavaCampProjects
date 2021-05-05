f53852d16b7e97033a8a83ba048f200925a6b386
Ayse ozavci <56216719+muhendisaysee@users.noreply.github.com>
2021-05-05 15:36:25 +0300

    Delete Homework3.2 directory

--- Homework3.2/src/InstructorManager.java
+++
@@ -1,7 +0,0 @@
-
-public class InstructorManager extends UserManager{
-
-	public void logIn() {
-		System.out.println("Eðitmen sistemine giriþ yapýldý");
-	}
-}
--- Homework3.2/src/StudentManager.java
+++
@@ -1,8 +0,0 @@
-
-public class StudentManager extends UserManager{
-
-	public void logIn() {
-		System.out.println("Ogrenci sistemine giriþ yapýldý");
-	}
-	
-}
